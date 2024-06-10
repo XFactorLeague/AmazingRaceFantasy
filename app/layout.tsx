518616c7e041faaacc5e95ddc06a19621a75b283
Jacob Meixner <jjm3333@gmail.com>
2024-06-10 13:09:12 -0700

    Add missing import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { getPages } from '@/app/utils/pages';
 
 const inter = Inter({ subsets: ['latin'] })
 
